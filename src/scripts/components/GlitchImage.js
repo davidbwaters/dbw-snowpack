f54646724ac81554433daf0114a492c12edac950
David B. Waters <mrdavidbwaters@gmail.com>
2020-08-21 22:46:44 -0400

    .

--- src/scripts/components/GlitchImage.js
+++ src/scripts/components/GlitchImage.js
@@ -1,6 +1,7 @@
 /*
- *  Components - Top Bar
+ *  Components - Glitch Image
  */
+
 import { LitElement, html, css } from 'lit-element'
 
 export class GlitchImage extends LitElement {
@@ -21,21 +22,8 @@ export class GlitchImage extends LitElement {
       :host:hover {
         cursor: pointer;
       }
-      
-      .c-glitch__img {
-        position: absolute;
-        top: calc(-1 * var(--gap-vertical));
-        left: calc(-1 * var(--gap-horizontal));
-        width: calc(100% + var(--gap-horizontal) * 2);
-        height: calc(100% + var(--gap-vertical) * 2);
-        background: url(../img/1.jpg) no-repeat 50% 0;
-        background-color: var(--blend-color-1);
-        background-size: cover;
-        transform: translate3d(0,0,0);
-        background-blend-mode: var(--blend-mode-1);
-      }
 
-      .c-glitch--style-1 {
+      :host(.c-glitch-image--style-1) {
         --gap-horizontal: 20px;
         --gap-vertical: 2px;
         --time-anim: 2.25s;
@@ -51,7 +39,7 @@ export class GlitchImage extends LitElement {
         --blend-color-5: transparent;
       }
       
-      .c-glitch--style-2 {
+      :host(.c-glitch-image--style-2) {
         --gap-horizontal: 5px;
         --gap-vertical: 10px;
         --time-anim: 2s;
@@ -67,7 +55,7 @@ export class GlitchImage extends LitElement {
         --blend-color-5: #c9b09a;
       }
       
-      .c-glitch--style-3 {
+      :host(.c-glitch-image--style-3) {
         --gap-horizontal: 20px;
         --gap-vertical: 2px;
         --time-anim: 2.25s;
@@ -83,7 +71,7 @@ export class GlitchImage extends LitElement {
         --blend-color-5: transparent;
       }
       
-      .c-glitch--style-4 {
+      :host(.c-glitch-image--style-4) {
         --gap-horizontal: 5px;
         --gap-vertical: 20px;
         --time-anim: 5s;
@@ -99,7 +87,7 @@ export class GlitchImage extends LitElement {
         --blend-color-5: transparent;
       }
       
-      .c-glitch--style-5 {
+      :host(.c-glitch-image--style-5) {
         --gap-horizontal: 50px;
         --gap-vertical: 100px;
         --time-anim: 2.25s;
@@ -115,7 +103,7 @@ export class GlitchImage extends LitElement {
         --blend-color-5: #8d16f2;
       }
       
-      .c-glitch--style-6 {
+      :host(.c-glitch-image--style-6) {
         --gap-horizontal: 3px;
         --gap-vertical: 70px;
         --time-anim: 2.25s;
@@ -130,22 +118,307 @@ export class GlitchImage extends LitElement {
         --blend-color-4: transparent;
         --blend-color-5: transparent;
       }
+
+      .c-glitch-image__image {
+        background-blend-mode: var(--blend-mode-1);
+        background-color: var(--blend-color-1);        
+        background-position: 50% 0;
+        background-repeat: no-repeat;
+        background-size: cover;
+        height: calc(100% + var(--gap-vertical) * 2);
+        left: calc(-1 * var(--gap-horizontal));
+        position: absolute;
+        top: calc(-1 * var(--gap-vertical));
+        transform: translate3d(0,0,0);        
+        width: calc(100% + var(--gap-horizontal) * 2);
+      }
+
+      .glitch__img:nth-child(2) {
+        background-color: var(--blend-color-2);
+        background-blend-mode: var(--blend-mode-2);
+      }
+
+      .glitch__img:nth-child(3) {
+        background-color: var(--blend-color-3);
+        background-blend-mode: var(--blend-mode-3);
+      }
+
+      .glitch__img:nth-child(4) {
+        background-color: var(--blend-color-4);
+        background-blend-mode: var(--blend-mode-4);
+      }
+
+      .glitch__img:nth-child(5) {
+        background-color: var(--blend-color-5);
+        background-blend-mode: var(--blend-mode-5);
+      }
+
+      .glitch__img:nth-child(n+2) {
+        opacity: 0;
+      }
+
+      .glitch:hover .glitch__img:nth-child(n+2) {
+        opacity: 1;
+      }
+
+      .glitch:hover .glitch__img:nth-child(2) {
+        transform: translate3d(var(--gap-horizontal),0,0);
+        animation: glitch-anim-1-horizontal var(--time-anim) infinite linear alternate;
+      }
+
+      .glitch:hover > .glitch__img:nth-child(3) {
+        transform: translate3d(calc(-1 * var(--gap-horizontal)),0,0);
+        animation: glitch-anim-2-horizontal var(--time-anim) infinite linear alternate;
+      }
+
+      .glitch:hover > .glitch__img:nth-child(4) {
+        transform: translate3d(0, calc(-1 * var(--gap-vertical)), 0) scale3d(-1,-1,1);
+        animation: glitch-anim-3-horizontal var(--time-anim) infinite linear alternate;
+      }
+
+      /* Hover flash animation on last image */
+      .glitch:hover > .glitch__img:nth-child(5) {
+        animation: glitch-anim-flash 0.5s steps(1,end) infinite;
+      }
+
+      @keyframes glitch-anim-1-horizontal {
+        0% { 
+          -webkit-clip-path: polygon(0 2%, 100% 2%, 100% 5%, 0 5%);
+          clip-path: polygon(0 2%, 100% 2%, 100% 5%, 0 5%);
+        }
+        10% {
+          -webkit-clip-path: polygon(0 15%, 100% 15%, 100% 15%, 0 15%);
+          clip-path: polygon(0 15%, 100% 15%, 100% 15%, 0 15%);
+        }
+        20% {
+          -webkit-clip-path: polygon(0 10%, 100% 10%, 100% 20%, 0 20%);
+          clip-path: polygon(0 10%, 100% 10%, 100% 20%, 0 20%);
+        }
+        30% {
+          -webkit-clip-path: polygon(0 1%, 100% 1%, 100% 2%, 0 2%);
+          clip-path: polygon(0 1%, 100% 1%, 100% 2%, 0 2%);
+        }
+        40% {
+          -webkit-clip-path: polygon(0 33%, 100% 33%, 100% 33%, 0 33%);
+          clip-path: polygon(0 33%, 100% 33%, 100% 33%, 0 33%);
+        }
+        50% {
+          -webkit-clip-path: polygon(0 44%, 100% 44%, 100% 44%, 0 44%);
+          clip-path: polygon(0 44%, 100% 44%, 100% 44%, 0 44%);
+        }
+        60% {
+          -webkit-clip-path: polygon(0 50%, 100% 50%, 100% 20%, 0 20%);
+          clip-path: polygon(0 50%, 100% 50%, 100% 20%, 0 20%);
+        }
+        70% {
+          -webkit-clip-path: polygon(0 70%, 100% 70%, 100% 70%, 0 70%);
+          clip-path: polygon(0 70%, 100% 70%, 100% 70%, 0 70%);
+        }
+        80% {
+          -webkit-clip-path: polygon(0 80%, 100% 80%, 100% 80%, 0 80%);
+          clip-path: polygon(0 80%, 100% 80%, 100% 80%, 0 80%);
+        }
+        90% {
+          -webkit-clip-path: polygon(0 50%, 100% 50%, 100% 55%, 0 55%);
+          clip-path: polygon(0 50%, 100% 50%, 100% 55%, 0 55%);
+        }
+        100% {
+          -webkit-clip-path: polygon(0 70%, 100% 70%, 100% 80%, 0 80%);
+          clip-path: polygon(0 70%, 100% 70%, 100% 80%, 0 80%);
+        }
+      }
+
+      @keyframes glitch-anim-2-horizontal {
+        0% { 
+          -webkit-clip-path: polygon(0 25%, 100% 25%, 100% 30%, 0 30%);
+          clip-path: polygon(0 25%, 100% 25%, 100% 30%, 0 30%);
+        }
+        15% {
+          -webkit-clip-path: polygon(0 3%, 100% 3%, 100% 3%, 0 3%);
+          clip-path: polygon(0 3%, 100% 3%, 100% 3%, 0 3%);
+        }
+        22% {
+          -webkit-clip-path: polygon(0 5%, 100% 5%, 100% 20%, 0 20%);
+          clip-path: polygon(0 5%, 100% 5%, 100% 20%, 0 20%);
+        }
+        31% {
+          -webkit-clip-path: polygon(0 20%, 100% 20%, 100% 20%, 0 20%);
+          clip-path: polygon(0 20%, 100% 20%, 100% 20%, 0 20%);
+        }
+        45% {
+          -webkit-clip-path: polygon(0 40%, 100% 40%, 100% 40%, 0 40%);
+          clip-path: polygon(0 40%, 100% 40%, 100% 40%, 0 40%);
+        }
+        51% {
+          -webkit-clip-path: polygon(0 52%, 100% 52%, 100% 59%, 0 59%);
+          clip-path: polygon(0 52%, 100% 52%, 100% 59%, 0 59%);
+        }
+        63% {
+          -webkit-clip-path: polygon(0 60%, 100% 60%, 100% 60%, 0 60%);
+          clip-path: polygon(0 60%, 100% 60%, 100% 60%, 0 60%);
+        }
+        76% {
+          -webkit-clip-path: polygon(0 75%, 100% 75%, 100% 75%, 0 75%);
+          clip-path: polygon(0 75%, 100% 75%, 100% 75%, 0 75%);
+        }
+        81% {
+          -webkit-clip-path: polygon(0 65%, 100% 65%, 100% 40%, 0 40%);
+          clip-path: polygon(0 65%, 100% 65%, 100% 40%, 0 40%);
+        }
+        94% {
+          -webkit-clip-path: polygon(0 45%, 100% 45%, 100% 50%, 0 50%);
+          clip-path: polygon(0 45%, 100% 45%, 100% 50%, 0 50%);
+        }
+        100% {
+          -webkit-clip-path: polygon(0 14%, 100% 14%, 100% 33%, 0 33%);
+          clip-path: polygon(0 14%, 100% 14%, 100% 33%, 0 33%);
+        }
+      }
+
+      @keyframes glitch-anim-3-horizontal {
+        0% { 
+          -webkit-clip-path: polygon(0 1%, 100% 1%, 100% 3%, 0 3%);
+          clip-path: polygon(0 1%, 100% 1%, 100% 3%, 0 3%);
+        }
+        5% {
+          -webkit-clip-path: polygon(0 10%, 100% 10%, 100% 9%, 0 9%);
+          clip-path: polygon(0 10%, 100% 10%, 100% 9%, 0 9%);
+        }
+        10% {
+          -webkit-clip-path: polygon(0 5%, 100% 5%, 100% 6%, 0 6%);
+          clip-path: polygon(0 5%, 100% 5%, 100% 6%, 0 6%);
+        }
+        25% {
+          -webkit-clip-path: polygon(0 20%, 100% 20%, 100% 20%, 0 20%);
+          clip-path: polygon(0 20%, 100% 20%, 100% 20%, 0 20%);
+        }
+        27% {
+          -webkit-clip-path: polygon(0 10%, 100% 10%, 100% 10%, 0 10%);
+          clip-path: polygon(0 10%, 100% 10%, 100% 10%, 0 10%);
+        }
+        30% {
+          -webkit-clip-path: polygon(0 30%, 100% 30%, 100% 25%, 0 25%);
+          clip-path: polygon(0 30%, 100% 30%, 100% 25%, 0 25%);
+        }
+        33% {
+          -webkit-clip-path: polygon(0 15%, 100% 15%, 100% 16%, 0 16%);
+          clip-path: polygon(0 15%, 100% 15%, 100% 16%, 0 16%);
+        }
+        37% {
+          -webkit-clip-path: polygon(0 40%, 100% 40%, 100% 39%, 0 39%);
+          clip-path: polygon(0 40%, 100% 40%, 100% 39%, 0 39%);
+        }
+        40% {
+          -webkit-clip-path: polygon(0 20%, 100% 20%, 100% 21%, 0 21%);
+          clip-path: polygon(0 20%, 100% 20%, 100% 21%, 0 21%);
+        }
+        45% {
+          -webkit-clip-path: polygon(0 60%, 100% 60%, 100% 55%, 0 55%);
+          clip-path: polygon(0 60%, 100% 60%, 100% 55%, 0 55%);
+        }
+        50% {
+          -webkit-clip-path: polygon(0 30%, 100% 30%, 100% 31%, 0 31%);
+          clip-path: polygon(0 30%, 100% 30%, 100% 31%, 0 31%);
+        }
+        53% {
+          -webkit-clip-path: polygon(0 70%, 100% 70%, 100% 69%, 0 69%);
+          clip-path: polygon(0 70%, 100% 70%, 100% 69%, 0 69%);
+        }
+        57% {
+          -webkit-clip-path: polygon(0 40%, 100% 40%, 100% 41%, 0 41%);
+          clip-path: polygon(0 40%, 100% 40%, 100% 41%, 0 41%);
+        }
+        60% {
+          -webkit-clip-path: polygon(0 80%, 100% 80%, 100% 75%, 0 75%);
+          clip-path: polygon(0 80%, 100% 80%, 100% 75%, 0 75%);
+        }
+        65% {
+          -webkit-clip-path: polygon(0 50%, 100% 50%, 100% 51%, 0 51%);
+          clip-path: polygon(0 50%, 100% 50%, 100% 51%, 0 51%);
+        }
+        70% {
+          -webkit-clip-path: polygon(0 90%, 100% 90%, 100% 90%, 0 90%);
+          clip-path: polygon(0 90%, 100% 90%, 100% 90%, 0 90%);
+        }
+        73% {
+          -webkit-clip-path: polygon(0 60%, 100% 60%, 100% 60%, 0 60%);
+          clip-path: polygon(0 60%, 100% 60%, 100% 60%, 0 60%);
+        }
+        80% {
+          -webkit-clip-path: polygon(0 100%, 100% 100%, 100% 99%, 0 99%);
+          clip-path: polygon(0 100%, 100% 100%, 100% 99%, 0 99%);
+        }
+        100% {
+          -webkit-clip-path: polygon(0 70%, 100% 70%, 100% 71%, 0 71%);
+          clip-path: polygon(0 70%, 100% 70%, 100% 71%, 0 71%);
+        }
+      }
+
+      @keyframes glitch-anim-flash {
+        0% { 
+          opacity: 0.2; 
+          transform: translate3d(var(--gap-horizontal), var(--gap-vertical), 0);
+        }
+        33%, 100% { 
+          opacity: 0;
+          transform: translate3d(0,0,0);
+        }
+      }      
     `
 
   }
 
   static get properties() {
 
+    return {
+      glitch: { type: String, attribute: true },
+      src: { type: String, attribute: true }
+    }
+
   }
+
+  constructor() {
+
+    super()
+
+    this.glitch = '1'
+
+  }
+
+  connectedCallback() {
+
+    this.classList.add('c-glitch-image--style-' + this.glitch)
+
+  }
+
   render() {
 
     return html`
-      <slot name="logo">
-      </slot>
-      <nav class="c-top-bar__nav">
-        <slot name="link">
-        </slot>
-      </nav>
+      <img 
+        src=#{this.src} 
+        class="c-glitch-image__image}"
+        style="background-image: url(${this.src});"
+      />
+      <img 
+        src=#{this.src} 
+        class="c-glitch-image__image}"
+        style="background-image: url(${this.src});"
+      />
+      <img 
+        src=#{this.src} 
+        class="c-glitch-image__image}"
+        style="background-image: url(${this.src});"
+      />
+      <img 
+        src=#{this.src} 
+        class="c-glitch-image__image}"
+        style="background-image: url(${this.src});"
+      />
+      <img 
+        src=#{this.src} 
+        class="c-glitch-image__image}"
+        style="background-image: url(${this.src});"
+      />
     `
 
   }
